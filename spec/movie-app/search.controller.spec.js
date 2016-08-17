d57a0c2432861afe5812c8716ba7dfab9b347dca
Jerrold Thompson <jet@whidbey.com>
2016-08-16 18:32:59 -0700

    extract function from spec to build search controller

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -1,21 +1,12 @@
 describe('Search Controller', function() {
   var $scope;
   var $location;
+  beforeEach(module('movieApp'));
   beforeEach(inject(function(_$controller_, _$location_) {
     $location = _$location_;
     $scope = {};
 
-    // extract out into a function which we can pass into _$controller_ service
-    var fn = function ($scope) {
-      $scope.search = function () {
-        if ($scope.query) {
-          $location.path('/results').search('q', $scope.query);
-          // $location.url = '/results?q=star%20wars';
-        }
-      }
-    }
-    // passing $location is optional since injector already providing this
-    _$controller_(fn, { $scope: $scope, $location: $location });
+    _$controller_('SearchController', { $scope: $scope, $location: $location });
   }));
   it('should redirect to the query results page for non-empty query', function() {
     $scope.query = 'star wars';
