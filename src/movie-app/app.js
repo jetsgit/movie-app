0fc7a16c2c87b3d36720e1030d7f842a8c1ad27f
Jerrold Thompson <jet@whidbey.com>
2016-08-19 22:05:23 -0700

    $interval service working, html content test failing??

--- src/movie-app/app.js
+++ src/movie-app/app.js
@@ -1,6 +1,10 @@
-angular.module('movieApp', ['omdb','ngRoute', 'ui.bootstrap'])
+angular.module('movieApp', ['omdb','ngRoute', 'ui.bootstrap', 'movieCore'])
   .config( function ($routeProvider) {
     $routeProvider
+      .when('/', {
+        templateUrl: 'movie-app/home.html',
+        controller: 'HomeController'
+      })
       .when('/results', {
         templateUrl: 'movie-app/results.html',
         controller: 'ResultsController'
